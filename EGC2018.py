b8c70e7bb98e027dbd9d1cb0f9ab668080f42f42
Jordan Peters <rollersteaam@googlemail.com>
2018-11-11 00:06:31 +0000

    Add doc-strings

--- EGC2018.py
+++ EGC2018.py
@@ -3,34 +3,49 @@
 
 # In[235]:
 
-
 import matplotlib.pyplot as plt
 import requests
 import pandas
 import matplotlib.patches as mpatches
 import pandas as pd
 
-
 # In[278]:
 
-
 def Firm_ID():
-    N_ID = int(input("How many firms do you want to plot? "))
-    IDs=[]
-    for i in range(N_ID):
-        IDs.append(input("Enter ID of the Firm: "))
-    return IDs
-IDs = Firm_ID()
+    '''
+    Requests the amount of firms a user wants to plot, requesting the IDs of each
+    individual plot after.
 
+    Returns:
+        (list) The Firm IDs the user wants.
+    '''
+    number_of_ids = int(input("How many firms do you want to plot? "))
+
+    IDs = []
+    for i in range(number_of_ids):
+        IDs.append(input("Enter ID of the Firm: "))
+
+    return IDs
+
+IDs = Firm_ID()
 
 # In[279]:
 
 
 def Line_Names(IDs):
+    '''
+    Makes a GET request to the instruments page and returns the symbols of all available instruments.
+
+    Args:
+        IDs (list): A list of all instrument IDs.
+
+    Returns:
+        (list) The symbols of all instruments.
+    '''
     marketdata = requests.get('http://egchallenge.tech/instruments').json()
     StacD = pandas.DataFrame(marketdata)
     All_Sym_List = StacD["symbol"].tolist()
-    Sym_List=[]
+    Sym_List = []
     for i in range(len(IDs)):
         Sym_List.append(All_Sym_List[int(IDs[i])-1])
 
@@ -46,7 +61,16 @@ Sym_List = Line_Names(IDs)
 
 
 def Prices(IDs, Sym_List):
-    
+    '''
+    Makes a GET request and returns the prices of all supplied 
+
+    Args:
+        IDs (list): List of IDs of the prices to request.
+        Sym_List (list): List of symbols that map to the list argument of IDs.
+
+    Returns:
+        (DataFrame): A table of IDs and prices combined together.
+    '''
     id_data = requests.get('http://egchallenge.tech/marketdata/instrument/'+IDs[0]).json()
     #pricedata=[json["price"] for json in id_data]
     Name = Sym_List[0]
@@ -61,6 +85,7 @@ def Prices(IDs, Sym_List):
         
     #print(Price_df)
     return Price_df
+
 Price_df = Prices(IDs, Sym_List)
 
 
@@ -68,7 +93,13 @@ Price_df = Prices(IDs, Sym_List)
 
 
 def graph(IDs, Sym_List):
-    
+    '''
+    Creates a line graph of epoch against instrument price.
+
+    Args:
+        IDs (list): The list of IDs of instruments to use in the graph.
+        Sym_List (list): The list of symbols that map to the IDs of the instruments.
+    '''
     for i in range(len(IDs)):
         plt.plot(Price_df)
 
